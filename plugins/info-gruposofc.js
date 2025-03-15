797802a712d0f867d777f15c0d1f63bde21b156b
@xrljose <xrljose105@gmail.com>
2025-03-15 17:09:56 -0500

    Update info-gruposofc.js

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -1,8 +1,11 @@
-let handler = async (m, { conn, usedPrefix, command }) => {
-  let grupos = `*Hola!, te invito a unirte a los grupos oficiales del Bot para convivir con la comunidad.....*
+import fetch from 'node-fetch'
+
+let handler  = async (m, { conn, usedPrefix, command }) => {
+
+let grupos = `*Hola!, te invito a unirte a los grupos oficiales del Bot para convivir con la comunidad.....*
 
    *_╭━━━⊜ ⌊• 1 •⌉_*
-  *_┃🌹❏ https://chat.whatsapp.com/I2g1kNUSxf75HmZp5Nr3Ua_*
+  *_┃🌹❏ https://chat.whatsapp.com/EteP5pnrAZC14y9wReGF1V_*
 *_╰━━━━━━━━━━━━━━━━⊜_*
 
    *_╭━━━⊜ ⌊• 2 •⌉_*
@@ -10,19 +13,18 @@ let handler = async (m, { conn, usedPrefix, command }) => {
 *_╰━━━━━━━━━━━━━━━━⊜_*
 
    *_╭━━━⊜ ⌊• 3 •⌉_*
-  *_┃🌹❏ https://chat.whatsapp.com/CX0KRkfViZS0jKZHkTJlCM_*
+  *_┃🌹❏ https://chat.whatsapp.com/DeJvBuS7QgB3Ybp1BZulWL_*
 *_╰━━━━━━━━━━━━━━━━⊜_*
 
    *_╭━━━⊜ ⌊• Canal •⌉_*
   *_┃🌹❏ https://whatsapp.com/channel/0029Vb4cQJu2f3EB7BS7o11M_*
 *_╰━━━━━━━━━━━━━━━━⊜_*`
 
-  const catalogo1 = 'https://qu.ax/NTXEi.mp4'
+await conn.sendFile(m.chat, catalogo, "grupos.jpg", grupos, m)
 
-  await conn.sendFile(m.chat, catalogo1, 'jose.jpg', grupos, m, rcanal)
-  await m.react(emojis)
+await m.react(🥛)
+
 }
-
 handler.help = ['grupos']
 handler.tags = ['info']
 handler.command = ['grupos', 'links', 'groups']
