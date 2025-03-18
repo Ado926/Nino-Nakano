a594a351c673c150afa1d178e800312e56009a28
@xrljose <xrljose105@gmail.com>
2025-03-18 06:18:39 -0500

    Update plaquinhas.js

--- plugins/plaquinhas.js
+++ plugins/plaquinhas.js
@@ -2,22 +2,22 @@ import fetch from 'node-fetch';
 
 const handler = async (m, { text, usedPrefix, command }) => {
     const apiMapping = {
-        plaq1: `https://api.nexfuture.com.br/api/plaquinhas/plaq1?query=${encodeURIComponent(text)}`,
-        plaq2: `https://api.nexfuture.com.br/api/plaquinhas/plaq2?query=${encodeURIComponent(text)}`,
-        plaq3: `https://api.nexfuture.com.br/api/plaquinhas/plaq3?query=${encodeURIComponent(text)}`,
-        plaq4: `https://api.nexfuture.com.br/api/plaquinhas/plaq4?query=${encodeURIComponent(text)}`,
-        plaq5: `https://api.nexfuture.com.br/api/plaquinhas/plaq5?query=${encodeURIComponent(text)}`,
-        plaq6: `https://api.nexfuture.com.br/api/plaquinhas/plaq6?query=${encodeURIComponent(text)}`,
-        plaq7: `https://api.nexfuture.com.br/api/plaquinhas/plaq7?query=${encodeURIComponent(text)}`,
-        plaq8: `https://api.nexfuture.com.br/api/plaquinhas/plaq8?query=${encodeURIComponent(text)}`,
-        plaq9: `https://api.nexfuture.com.br/api/plaquinhas/plaq9?query=${encodeURIComponent(text)}`,
-        plaq10: `https://api.nexfuture.com.br/api/plaquinhas/plaq10?query=${encodeURIComponent(text)}`
+        plaq1: `https://carisys.online/api/plaquinhas/plaq1?query=${encodeURIComponent(text)}`,
+        plaq2: `https://carisys.online/api/plaquinhas/plaq2?query=${encodeURIComponent(text)}`,
+        plaq3: `https://carisys.online/api/plaquinhas/plaq3?query=${encodeURIComponent(text)}`,
+        plaq4: `https://carisys.online/api/plaquinhas/plaq4?query=${encodeURIComponent(text)}`,
+        plaq5: `https://carisys.online/api/plaquinhas/plaq5?query=${encodeURIComponent(text)}`,
+        plaq6: `https://carisys.online/api/plaquinhas/plaq6?query=${encodeURIComponent(text)}`,
+        plaq7: `https://carisys.online/api/plaquinhas/plaq7?query=${encodeURIComponent(text)}`,
+        plaq8: `https://carisys.online/api/plaquinhas/plaq8?query=${encodeURIComponent(text)}`,
+        plaq9: `https://carisys.online/api/plaquinhas/plaq9?query=${encodeURIComponent(text)}`,
+        plaq10: `https://carisys.online/api/plaquinhas/plaq10?query=${encodeURIComponent(text)}`
     };
 
     const apiUrl = apiMapping[command];
 
     if (!text || !apiUrl) {
-        return conn.reply(m.chat, `☕️ *Por favor ingresa una texto para convertira su plaquinha *\n\nEjemplo: ${usedPrefix + command} Jose`, m, rcanal);
+        return conn.reply(m.chat, `🍬 *Por favor ingresa una texto para convertira su plaquinha*\n\nEjemplo: ${usedPrefix + command} Jose`, m, rcanal);
     }
 
     try {
