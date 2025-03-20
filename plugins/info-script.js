8a5c219316bbe9693d3c45451d6fbe04d191b220
@xrljose <xrljose105@gmail.com>
2025-03-19 19:47:04 -0500

    Add files via upload

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -1,66 +1,33 @@
-//Codigo creado por ivan y José elver 
-import fetch from 'node-fetch';
+import moment from 'moment-timezone'
+import fetch from 'node-fetch'
 
-let handler = async (m, { conn, usedPrefix, command }) => {
-    const githubRepoURL = 'https://github.com/JoseXrl15k/Nino-Nakano';
+let handler = async (m, { conn, args }) => {
+try {
+let res = await fetch('https://api.github.com/repos/JoseXrl15k/Nino-Nakano')
 
-    try {
-        const [, username, repoName] = githubRepoURL.match(/github\.com\/([^/]+)\/([^/]+)/);
-        const response = await fetch(`https://api.github.com/repos/${username}/${repoName}`);
+if (!res.ok) throw new Error('Error al obtener datos del repositorio')
+let json = await res.json()
 
-        if (!response.ok) {
-            throw new Error(`La solicitud a la API de GitHub falló con el estado ${response.status}`);
-        }
+let txt = `*乂  S C R I P T  -  M A I N  乂*\n\n`
+txt += `✩  *Nombre* : ${json.name}\n`
+txt += `✩  *Visitas* : ${json.watchers_count}\n`
+txt += `✩  *Peso* : ${(json.size / 1024).toFixed(2)} MB\n`
+txt += `✩  *Actualizado* : ${moment(json.updated_at).format('DD/MM/YY - HH:mm:ss')}\n`
+txt += `✩  *Url* : ${json.html_url}\n`
+txt += `✩  *Forks* : ${json.forks_count}\n`
+txt += `✩  *Stars* : ${json.stargazers_count}\n\n`
+txt += `> *${dev}*`
 
-        const repoData = await response.json();
-        let Codes = '`📂  I N F O R M A C I Ó N  D E  R E P O S I T O R I O`\n\n';
-        Codes += `    ✩  *NOMBRE DEL BOT* : ${repoData.name}\n`;
-        Codes += `    ✩  *NOMBRE DEL PROPIETARIO* : ${repoData.owner.login}\n`;
-        Codes += `    ✩  *ESTRELLAS* : ${repoData.stargazers_count}\n`;
-        Codes += `    ✩  *FORKS* : ${repoData.forks_count}\n`;
-        Codes += `    ✩  *ENLACE DE GITHUB* : ${repoData.html_url}\n`;
-        Codes += `    ✩  *DESCRIPCIÓN* : ${repoData.description || 'Sin descripción disponible'}\n\n`;
-        Codes += `*¡No olvides darle una estrella y hacer fork al repositorio!*`;
+await conn.sendMessage(m.chat, {text: txt, contextInfo: { forwardingScore: 999, isForwarded: true, forwardedNewsletterMessageInfo: { newsletterName: channelRD.name, newsletterJid: channelRD.id, }, externalAdReply: { title: packname, body: dev, thumbnailUrl: 'https://qu.ax/uhJii.jpg', sourceUrl: redes, mediaType: 1, renderLargerThumbnail: true }}}, {quoted: fkontak})
 
-        await conn.sendMessage(m.chat, {
-            image: { url: 'https://qu.ax/uhJii.jpg' },
-            caption: Codes,
-            contextInfo: { 
-                mentionedJid: [m.sender],
-                forwardingScore: 999,
-                isForwarded: true,
-                forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363377833048768@newsletter',
-                    newsletterName: '┊  ✿    𝙽͜𝙸͜𝙽͜𝙾 𝙽͜𝙰͜𝙺𝙰𝙽͜𝙾    ░    ꒰ 𝙲ʜᴀɴɴᴇʟ ꒱    🍁̷⪨',
-                    serverMessageId: 143
-                }
-            }
-        }, { quoted: m });
+} catch {
+await conn.reply(m.chat, `${msm} Ocurrió un error.`, m)
+await m.react(error)
+}}
 
-        await conn.sendMessage(m.chat, {
-            audio: { url: 'https://github.com/JawadYTX/KHAN-DATA/raw/refs/heads/main/autovoice/repo.m4a' },
-            mimetype: 'audio/mp4',
-            ptt: true,
-            contextInfo: { 
-                mentionedJid: [m.sender],
-                forwardingScore: 999,
-                isForwarded: true,
-                forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363377833048768@newsletter',
-                    newsletterName: '┊  ✿    𝙽͜𝙸͜𝙽͜𝙾 𝙽͜𝙰͜𝙺𝙰𝙽͜𝙾    ░    ꒰ 𝙲ʜᴀɴɴᴇʟ ꒱    🍁̷⪨',
-                    serverMessageId: 143
-                }
-            }
-        }, { quoted: m });
+handler.help = ['script']
+handler.tags = ['info']
+handler.command = ['script', 'sc']
+handler.register = true
 
-    } catch (error) {
-        await conn.reply(m.chat, "Lo siento, ocurrió un error al obtener la información del repositorio. Por favor, intenta de nuevo más tarde.", m);
-    }
-}
-
-handler.tags = ['info'];
-handler.help = ['repo', 'sc', 'script', 'archivo'];
-handler.command = ['repo', 'sc', 'script', 'archivo'];
-handler.register = true;
-
-export default handler;
+export default handler
