7afefa6a05635e4db2b659fed4b02ce85f495f6c
@xrljose <xrljose105@gmail.com>
2025-03-21 17:33:50 -0500

    Add files via upload

--- plugins/owner-exec2.js
+++ plugins/owner-exec2.js
@@ -1,25 +1,24 @@
-import cp, {exec as _exec} from 'child_process';
-import {promisify} from 'util';
-const exec = promisify(_exec).bind(cp);
-const handler = async (m, {conn, isOwner, command, text, usedPrefix, args, isROwner}) => {
-  if (!isROwner) return;
-  if (global.conn.user.jid != conn.user.jid) return;
-  m.reply(`${emoji} *Ejecutando...*`);
-  let o;
-  try {
-    o = await exec(command.trimStart() + ' ' + text.trimEnd());
-  } catch (e) {
-    o = e;
-  } finally {
-    const {stdout, stderr} = o;
-    if (stdout.trim()) m.reply(stdout);
-    if (stderr.trim()) m.reply(stderr);
-  }
-};
+import cp, { exec as _exec } from 'child_process'
+import { promisify } from 'util'
+let exec = promisify(_exec).bind(cp)
+let handler = async (m, { conn, isOwner, isROwner, command, text }) => {
+if (!isROwner) return
+if (conn.user.jid != conn.user.jid) return
+m.react("💻")
+let o
+try {
+o = await exec(command.trimStart()  + ' ' + text.trimEnd())
+} catch (e) {
+o = e
+} finally {
+let { stdout, stderr } = o
+if (stdout.trim()) m.reply(stdout)
+if (stderr.trim()) m.reply(stderr)
+}}
 handler.help = ['$']
-handler.tags = ['owner']
-handler.customPrefix = ['$']
+handler.tags = ['owner'];
+handler.customPrefix = /^[$] /
 handler.command = new RegExp
-handler.rowner = true
+//handler.rowner = true
 
 export default handler
