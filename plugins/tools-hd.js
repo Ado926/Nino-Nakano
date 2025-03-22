5a7e105602be064810b6786af0ead96bf1a21e00
@xrljose <xrljose105@gmail.com>
2025-03-22 17:20:41 -0500

    Add files via upload

--- plugins/tools-hd.js
+++ plugins/tools-hd.js
@@ -1,53 +1,60 @@
-import FormData from "form-data";
-import Jimp from "jimp";
-const handler = async (m, {conn, usedPrefix, command}) => {
- try {    
-  let q = m.quoted ? m.quoted : m;
-  let mime = (q.msg || q).mimetype || q.mediaType || "";
-  if (!mime) return m.reply(`${emoji} Por favor, responda a una imagen para aunmetar el *HD*.`);
-  if (!/image\/(jpe?g|png)/.test(mime)) return m.reply(`${emoji2} El formato del archivo (${mime}) no es compatible, envía o responda a una imagen.`);
-  conn.reply(m.chat, `${emoji2} Mejorando la calidad de la imagen....`, m, {
-  contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
-  title: packname,
-  body: wm,
-  previewType: 0, thumbnail: icons,
-  sourceUrl: channel }}})
-  let img = await q.download?.();
-  let pr = await remini(img, "enhance");
-  conn.sendMessage(m.chat, {image: pr}, {quoted: fkontak});
- } catch {
- return m.reply(`${msm} Ocurrió un error.`);
- }
-};
-handler.help = ["remini", "hd", "enhance"];
-handler.tags = ["tools"];
-handler.group = true;
-handler.register = true
-handler.command = ["remini", "hd", "enhance"];
-export default handler;
+import fetch from 'node-fetch'
 
-async function remini(imageData, operation) {
-  return new Promise(async (resolve, reject) => {
-    const availableOperations = ["enhance", "recolor", "dehaze"];
-    if (availableOperations.includes(operation)) {
-      operation = operation;
-    } else {
-      operation = availableOperations[0];
+async function Escalar(imagenBuffer) {
+    try {
+        const response = await fetch("https://lexica.qewertyy.dev/upscale", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                image_data: imagenBuffer.toString("base64"), 
+                format: "binary",
+            }),
+        })
+
+        return Buffer.from(await response.arrayBuffer())
+    } catch {
+        return null
     }
-    const baseUrl = "https://inferenceengine.vyro.ai/" + operation + ".vyro";
-    const formData = new FormData();
-    formData.append("image", Buffer.from(imageData), {filename: "enhance_image_body.jpg", contentType: "image/jpeg"});
-    formData.append("model_version", 1, {"Content-Transfer-Encoding": "binary", contentType: "multipart/form-data; charset=utf-8"});
-    formData.submit({url: baseUrl, host: "inferenceengine.vyro.ai", path: "/" + operation, protocol: "https:", headers: {"User-Agent": "okhttp/4.9.3", Connection: "Keep-Alive", "Accept-Encoding": "gzip"}},
-      function (err, res) {
-        if (err) reject(err);
-        const chunks = [];
-        res.on("data", function (chunk) {chunks.push(chunk)});
-        res.on("end", function () {resolve(Buffer.concat(chunks))});
-        res.on("error", function (err) {
-        reject(err);
-        });
-      },
-    );
-  });
 }
+
+let manejador = async (m, { conn, usedPrefix, command }) => {
+    conn.mejorador = conn.mejorador || {}
+
+    if (m.sender in conn.mejorador)
+        throw "🍬Hay un proceso en curso. Por favor espera."
+
+    let q = m.quotes ? m.quotes : m
+    let mime = (q.msg || q).mimetype || q.mediaType || ""
+    if (!mime) throw "🍬Envía/Responde con una foto."
+    if (!/image\/(jpe?g|png)/.test(mime)) throw `🍬 El tipo ${mime} no es compatible.`
+
+    conn.mejorador[m.sender] = true
+    await conn.sendMessage(m.chat, { react: { text: "🧃", key: m.key } })
+
+    let img = await q.download?.()
+    let imgMejorada = await Escalar(img)
+
+    if (imgMejorada) {
+        await conn.sendMessage(m.chat, { react: { text: "✅", key: m.key } })
+        conn.sendFile(
+            m.chat,
+            imgMejorada,
+            "",
+            "Hecho ✅",
+            m
+        )
+    } else {
+        await conn.sendMessage(m.chat, { react: { text: "❌", key: m.key } })
+        m.reply("*Resultado:* Fallido ");
+    }
+
+    delete conn.mejorador[m.sender]
+}
+
+manejador.help = ["remini"]
+manejador.tags = ["remini"]
+manejador.command = /^(hd|remini)$/i
+
+export default manejador
